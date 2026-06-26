fc53c289c431e552a544d486e0378b864c632c3c
Antonio de Jesús Muñoz Muñoz <munozmunozantoniodejesus@gmail.com>
2026-06-25 18:08:08 -0600

    ahora se puede crear un grafo, guardarlo, y cargarlo

---
+++ lib/data/db.dart
@@ -0,0 +1,74 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class taskStorageDBHelper {
+  static final taskStorageDBHelper instance = taskStorageDBHelper._init();
+  static Database? _db;
+
+  taskStorageDBHelper._init();
+
+   static Future<Database> getDB() async{
+      //if it doesnt exist, initialize it
+      if (_db == null){
+        _db = await _initDB();
+      }
+
+      return _db!;
+   }
+   
+   static Future<Database> _initDB() async{
+    //get path
+    final pathToDB = join(await getDatabasesPath(), 'taskStorageDB.db');
+
+    //return the database in that path
+    return await openDatabase(pathToDB, version: 1, onCreate: _onCreate);
+
+   }
+
+
+
+
+   //on create method for the openDatabase function, basically the querys to create the db
+   static Future<void> _onCreate(Database db, int version) async {
+      await db.transaction( ((txn) async {
+        
+        //table for storing the jsons of the graphs
+        //im using the json format from the graph to json adding ny extra data i need to a node through the data field and a custom serializeable class
+        await txn.execute(
+          '''
+          CREATE TABLE graphsAsJson(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            graph TEXT
+          );
+          '''
+        );
+
+
+      })
+        
+      );
+   }
+
+  //interface
+   static Future<void> saveGraph(String graphJson, {int id = 1}) async {
+      var db = await getDB();
+
+     await db.transaction((txn) async {
+        await txn.insert('graphsAsJson', {'graph':graphJson, 'id':id}, conflictAlgorithm: ConflictAlgorithm.replace);
+      });
+   }
+
+   static Future<String> loadGraph({int id = 1}) async {
+      //load data
+      var db = await getDB();
+      var resultList = await db.query('graphsAsJson', where: 'id == ?', whereArgs: [id]);
+      
+      //if empty, return empy dict
+      if (resultList.isEmpty){
+        return '{"nodes":[],"connections":[],"viewport":{"x":0.0,"y":0.0,"zoom":1.0},"metadata":{}}';
+      }
+
+      //else return the actual data
+      return resultList[0]['graph'] as String;
+   }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
+import 'dart:convert';
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:master_planner/data/db.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'package:uuid/uuid.dart';
 import 'package:vyuh_node_flow/vyuh_node_flow.dart';
 
@@ -9,12 +15,15 @@ String genUniqueID() {return uuid.v4();}
 
 //Main func
 void main() {
+  sqfliteFfiInit();
+  databaseFactory = databaseFactoryFfi;
   runApp(const MainApp());
 }
 
 //Main app
 class MainApp extends StatefulWidget {
  const  MainApp({super.key});
+
 
   @override
   State<MainApp> createState() => _MainAppState();
@@ -27,62 +36,85 @@ class _MainAppState extends State<MainApp> {
   //the controller necesary for the tree view
   //it manages all state for it (nodes, connections, selections and viewport)
   // Create controller with initial nodes and connections
-  late final controller = NodeFlowController<String, dynamic>(
-    nodes: [
-      Node<String>(
-        id: 'start',
-        type: 'input',
-        position: const Offset(100, 100),
-        size: const Size(140, 70),
-        data: 'Start',
-        ports: [Port(id: 'id', name: 'name')]
-      ),
-      Node<String>(
-        id: 'process',
-        type: 'default',
-        position: const Offset(320, 100),
-        size: const Size(140, 70),
-        data: 'Process',
-      ),
-      Node<String>(
-        id: 'end',
-        type: 'output',
-        position: const Offset(540, 100),
-        size: const Size(140, 70),
-        data: 'End',
-      ),
-    ],
-    connections: [
-      Connection(
-        id: 'conn-1',
-        sourceNodeId: 'start',
-        sourcePortId: 'out',
-        targetNodeId: 'process',
-        targetPortId: 'in',
-      ),
-      Connection(
-        id: 'conn-2',
-        sourceNodeId: 'process',
-        sourcePortId: 'out',
-        targetNodeId: 'end',
-        targetPortId: 'in',
-      ),
-    ],
+  late final controller = NodeFlowController<dynamic, dynamic>(
+    nodes: []
+    //   Node<String>(
+    //     id: 'start',
+    //     type: 'input',
+    //     position: const Offset(100, 100),
+    //     size: const Size(140, 70),
+    //     data: 'Start',
+    //     ports: [Port(id: 'id', name: 'name')]
+    //   ),
+    //   Node<String>(
+    //     id: 'process',
+    //     type: 'default',
+    //     position: const Offset(320, 100),
+    //     size: const Size(140, 70),
+    //     data: 'Process',
+    //   ),
+    //   Node<String>(
+    //     id: 'end',
+    //     type: 'output',
+    //     position: const Offset(540, 100),
+    //     size: const Size(140, 70),
+    //     data: 'End',
+    //   ),
+    // ],
+    // connections: [
+    //   Connection(
+    //     id: 'conn-1',
+    //     sourceNodeId: 'start',
+    //     sourcePortId: 'out',
+    //     targetNodeId: 'process',
+    //     targetPortId: 'in',
+    //   ),
+    //   Connection(
+    //     id: 'conn-2',
+    //     sourceNodeId: 'process',
+    //     sourcePortId: 'out',
+    //     targetNodeId: 'end',
+    //     targetPortId: 'in',
+    //   ),
+    // ],
   );
 
+  //====================== nav bar actions ================================
   void _addNode(){
+    var nodeSize = Size(200,100);
     controller.addNode(
-                  Node(id: genUniqueID(), 
+                  Node<dynamic>(
+                    id: genUniqueID(), 
                     type: 'default', position: Offset(100, 100), 
-                    data: 'RAAA',
+                    data: 'testies',//TextField( decoration: null, textAlign: .center,),//TODO change to a serializable custom class
+                    size: nodeSize,
                     ports: [
-                      Port(id: genUniqueID(), name: 'asf', type: PortType.input,position: PortPosition.top, offset: Offset(100, 0)),
-                      Port(id: genUniqueID(), name: 'asf2', type: PortType.output, position: PortPosition.top),
-                      Port(id: genUniqueID(), name: 'asf3', type: PortType.input,position: PortPosition.bottom),
-                      Port(id: genUniqueID(), name: 'asf4', type: PortType.output, position: PortPosition.bottom)
+                      Port(id: genUniqueID(), name: 'asf', type: PortType.input,position: PortPosition.top, offset: Offset(nodeSize.width/2,0), multiConnections: true),
+                      Port(id: genUniqueID(), name: 'asf2', type: PortType.output, position: PortPosition.top, offset: Offset(nodeSize.width/2,0), multiConnections: true),
+                      Port(id: genUniqueID(), name: 'asf3', type: PortType.input,position: PortPosition.bottom, offset: Offset(nodeSize.width/2,0), multiConnections: true),
+                      Port(id: genUniqueID(), name: 'asf4', type: PortType.output, position: PortPosition.bottom, offset: Offset(nodeSize.width/2,0), multiConnections: true)
                       ]
                   )
                   );
+  }
+
+  void _saveGraph() async{
+    taskStorageDBHelper.saveGraph(controller.exportGraph().toJsonString());
+  }
+
+  void _loadGraph() async{
+    log(await taskStorageDBHelper.loadGraph());
+    controller.loadGraph(
+      NodeGraph.fromJson(
+        jsonDecode(await taskStorageDBHelper.loadGraph()),
+        (data) => data,
+        (data) => data
+      )
+    );
+  }
+
+  void test(){
+    log(controller.exportGraph().toJsonString());
   }
 
   @override
@@ -94,14 +126,20 @@ class _MainAppState extends State<MainApp> {
             mainAxisAlignment: .center,children: [ 
               IconButton(
                 onPressed: _addNode
-                ,icon: Icon(Icons.add))
+                ,icon: Icon(Icons.add)),
+              IconButton(onPressed: _saveGraph, icon: Icon(Icons.save)),
+              IconButton(onPressed: _loadGraph, icon: Icon(Icons.save_alt)),
+              IconButton(onPressed: test, icon: Icon(Icons.telegram))
             ],
           )
         ),
-        body: NodeFlowEditor<String, dynamic>(
+        body: NodeFlowEditor<dynamic, dynamic>(
                  controller: controller,
                  theme: NodeFlowTheme.light, 
-                 nodeBuilder: (context,node) => Center(child: Text(node.data))),
+                 nodeBuilder: (context,node) => Center(
+                    child: node.data is String? Text(node.data) : node.data
+                    )
+                  ),
       ),
     )
         ;
