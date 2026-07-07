0a279aa0716e07d56bbe0d8640233c3d21432fc7
Antonio de Jesús Muñoz Muñoz <munozmunozantoniodejesus@gmail.com>
2026-07-07 12:34:54 -0600

    killed the graph import

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,10 @@ import 'dart:convert';
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
-import 'package:master_planner/TimelineRuler.dart';
 import 'package:master_planner/data/db.dart';
 import 'package:master_planner/data/model.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'package:uuid/uuid.dart';
-import 'package:vyuh_node_flow/vyuh_node_flow.dart';
 
 var uuid = Uuid();
 enum Views {graph,tabbedText,stratifiedGraph}
@@ -76,7 +74,7 @@ class _MainAppState extends State<MainApp> {
 
 //graph view
 class GraphTaskView extends StatefulWidget {
-  GraphTaskView({
+  const GraphTaskView({
     super.key,
   });
 
@@ -86,57 +84,20 @@ class GraphTaskView extends StatefulWidget {
 
 class _GraphTaskViewState extends State<GraphTaskView> {
 
-  //the controller necesary for the tree view
-  //it manages all state for it (nodes, connections, selections and viewport)
-  // Create controller with initial nodes and connections
-  var nodeFlowCon = NodeFlowController<TaskModel, dynamic>(
-    config: NodeFlowConfig(
-      plugins: [
-        SnapPlugin([
-          GridSnapDelegate(gridSize: 50.0)
-        ],
-        enabled: false
-        )
-      ]
-    )
-  );
-
   //============================================= nav bar actions ==========================================
   void _addNode(){
-    var nodeSize = Size(200,100);
-    nodeFlowCon.addNode(
-                  Node<TaskModel>(
-                    id: genUniqueID(), 
-                    type: 'default', position: Offset(100, 100), 
-                    data: TaskModel("taskTitle", null, null),
-                    size: nodeSize,
-                    ports: [
-                      Port(id: genUniqueID(), name: 'asf', type: PortType.input,position: PortPosition.top, offset: Offset(nodeSize.width/2,0), multiConnections: true),
-                      Port(id: genUniqueID(), name: 'asf2', type: PortType.output, position: PortPosition.top, offset: Offset(nodeSize.width/2,0), multiConnections: true),
-                      Port(id: genUniqueID(), name: 'asf3', type: PortType.input,position: PortPosition.bottom, offset: Offset(nodeSize.width/2,0), multiConnections: true),
-                      Port(id: genUniqueID(), name: 'asf4', type: PortType.output, position: PortPosition.bottom, offset: Offset(nodeSize.width/2,0), multiConnections: true)
-                      ]
-                  )
-                  );
+    
   }
 
   void _saveGraph() async{
-    taskStorageDBHelper.saveGraph(nodeFlowCon.exportGraph().toJsonString());
+
   }
 
   void _loadGraph() async{
-    log(await taskStorageDBHelper.loadGraph());
-    nodeFlowCon.loadGraph(
-      NodeGraph.fromJson(
-        jsonDecode(await taskStorageDBHelper.loadGraph()),
-        (data) => TaskModel.fromJson(data as Map<String,dynamic>),
-        (data) => data
-      )
-    );
   }
 
   void test(){
-    log(nodeFlowCon.exportGraph().toJsonString());
+
   }
 
   @override
@@ -150,7 +111,7 @@ class _GraphTaskViewState extends State<GraphTaskView> {
                   ,icon: Icon(Icons.add)),
                 IconButton(onPressed: _saveGraph, icon: Icon(Icons.save)),
                 IconButton(onPressed: _loadGraph, icon: Icon(Icons.save_alt)),
-                IconButton(onPressed: () {(nodeFlowCon.plugins[0] as SnapPlugin).toggle();}, icon: Icon(Icons.timer)),//TODO this way of toggling stratos is SUPER hacky and will braek if another plugin ends up being the first one
+                IconButton(onPressed: () {}, icon: Icon(Icons.timer)),
                 IconButton(onPressed: test, icon: Icon(Icons.telegram))
             
               ],
@@ -160,39 +121,18 @@ class _GraphTaskViewState extends State<GraphTaskView> {
             child: Row(
               children: [
                 Expanded(
-                  child: NodeFlowEditor<TaskModel, dynamic>(
-                       controller: nodeFlowCon,
-                       theme: NodeFlowTheme.light, 
-                       nodeBuilder: (context,node) => Center(
-                          child: TextField(
-                            onChanged: (newData) =>{ node.data.title = newData}, 
-                            controller: TextEditingController(text: node.data.title),
-                            textAlign: .center,),
-                          ),
-                       events: NodeFlowEvents(
-                        viewport: ViewportEvents(
-                          onMove: (viewPortState) {
-                            log("message");
-                          },
-                        )
-                       ),
-                     ),
+                  child:SizedBox()
                 ),
-                   //TimeRuler(canvasOffsetY: 0, zoom: 1, epoch: DateTime(2026,6,27), pxPerHour: 10),
-                      
-                ]
+                           
+              ]
             ),
-
-                   
-          ),
-      
+          ),    
         ],
     );
   }
 
   @override
   void dispose(){
-    nodeFlowCon.dispose();
     super.dispose();
   }
 }
